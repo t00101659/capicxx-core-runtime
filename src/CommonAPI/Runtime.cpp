a487d93eda752360b31e7e18c9f742455c474397
t00101659 <290432146@qq.com>
2022-11-20 18:47:43 +0800

    default bounding modify for someip

--- src/CommonAPI/Runtime.cpp
+++ src/CommonAPI/Runtime.cpp
@@ -21,7 +21,7 @@
 
 namespace CommonAPI {
 
-const char *COMMONAPI_DEFAULT_BINDING = "dbus";
+const char *COMMONAPI_DEFAULT_BINDING = "someip";
 const char *COMMONAPI_DEFAULT_FOLDER = "/usr/local/lib/commonapi";
 const char *COMMONAPI_DEFAULT_CONFIG_FILE = "commonapi.ini";
 const char *COMMONAPI_DEFAULT_CONFIG_FOLDER = "/etc";
